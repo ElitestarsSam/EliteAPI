7e8230bb1adf5acc253116815e937caf2da41b4e
ElitestarsSam <62185235+ElitestarsSam@users.noreply.github.com>
2021-03-16 17:26:57 +0000

    Added startup

--- src/main/java/tech/bedev/eliteapi/API.java
+++ src/main/java/tech/bedev/eliteapi/API.java
@@ -1,5 +1,6 @@
 package tech.bedev.eliteapi;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
@@ -24,5 +25,25 @@ public final class API {
 
     public static void registerEvents(Listener listener) {
         jplugin.getServer().getPluginManager().registerEvents(listener, jplugin);
+    }
+
+    public static void startup(String pluginName, String enablingDisabling, String discordUrl, String authors, String version, String pluginUrl, String wikiUrl) {
+        Bukkit.getConsoleSender().sendMessage(msg(""));
+        Bukkit.getConsoleSender().sendMessage(msg("&8[]=====[" + enablingDisabling + " " + pluginName + " &cfrom BE Development&8]=====[]"));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|"));
+        Bukkit.getConsoleSender().sendMessage(msg("&8| &4Information:"));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|"));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|   &cName: " + pluginName));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|   &cDevelopers: " + authors));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|   &cVersion: " + version));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|   &cPlugin: " + pluginUrl));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|"));
+        Bukkit.getConsoleSender().sendMessage(msg("&8| &4Support:"));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|"));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|   &cWiki: " + wikiUrl));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|   &cDiscord: " + discordUrl));
+        Bukkit.getConsoleSender().sendMessage(msg("&8|"));
+        Bukkit.getConsoleSender().sendMessage(msg("&8[]===============================================[]"));
+        Bukkit.getConsoleSender().sendMessage(msg(""));
     }
 }
